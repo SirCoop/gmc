dba1ae362b15acc595b621ac1861b12342abc399
Gary Cooper <cooper.garym@gmail.com>
2018-07-22 11:29:32 -0400

    location dot on map

--- src/app/components/worldmap/worldmap.component.ts
+++ src/app/components/worldmap/worldmap.component.ts
@@ -71,18 +71,18 @@ export class WorldmapComponent implements OnInit {
       .attr('d', geoPath);
     
     // add circles to svg
-    // const visitedLocationCoordinates = [
-    //   [],
-    //   []
-    // ];
-  
-    // svg.selectAll('circle')
-    //   .data(visitedLocationCoordinates).enter()
-    //   .append('circle')
-    //   .attr('cx', function (d) { console.log(projection(d)); return projection(d)[0]; })
-    //   .attr('cy', function (d) { return projection(d)[1]; })
-    //   .attr('r', '8px')
-    //   .attr('fill', 'red');
+    const visitedLocationCoordinates: any = [
+      [-122.490402, 37.786453],
+      [-122.389809, 37.72728]
+    ];
+
+    svg.selectAll('circle')
+      .data(visitedLocationCoordinates).enter()
+      .append('circle')
+      .attr('cx', (d: any) => projection(d)[0])
+      .attr('cy', (d: any) => projection(d)[1])
+      .attr('r', '8px')
+      .attr('fill', 'red');
   }
 
   constructor(private renderer: Renderer2) { }
