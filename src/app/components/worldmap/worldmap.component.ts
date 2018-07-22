6fe5695aa9e17c205e355f1934d4b4daa60fe5be
Gary Cooper <cooper.garym@gmail.com>
2018-07-22 11:16:23 -0400

    colored map

--- src/app/components/worldmap/worldmap.component.ts
+++ src/app/components/worldmap/worldmap.component.ts
@@ -28,8 +28,8 @@ export class WorldmapComponent implements OnInit {
   appendData() {
 
     const margin = {top: 0, right: 0, bottom: 0, left: 0};
-    const width = 960 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
+    const width = 1080 - margin.left - margin.right;
+    const height = 720 - margin.top - margin.bottom;
    
     const color = d3.scaleThreshold()
       .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000]);
@@ -42,7 +42,7 @@ export class WorldmapComponent implements OnInit {
       .attr('class', 'map');
   
     const projection = d3.geoMercator()
-      .scale( 130 )  // try 130 or 190000
+      .scale( 130 )
       .rotate( [71.057, 0] )
       .center( [0, 42.313] )
       .translate( [width / 2, height / 1.5] );
@@ -50,46 +50,39 @@ export class WorldmapComponent implements OnInit {
     const geoPath = d3.geoPath()
       .projection( projection );
 
-    // create featureCollection
-    // const newCountries = topojson.feature(worldData, worldData.objects.countries);
-
-    // data looks same as topojson.feature() but without type error
-    const xform = this.getFeatures(worldData, 'countries');
-    
-    console.log('xform: ', xform);
-
-
-    // appends one path per country which is actually progress
-    // svg
-    //   .attr('class', 'countries')
-    //   .selectAll('path')
-    //   .data(xform)
-    //   .enter()
-    //   .append('path')
-    //   .attr('d', geoPath);
-
+    // create FeatureCollection
+    const countries: any = topojson.feature(worldData, worldData.objects.countries);
+    // get features
+    const countryFeatures: any = countries.features;
     // add states from topojson
-  svg.selectAll('path')
-    .data(worldData).enter()
-    .append('path')
-    .attr('class', 'feature')
-    .style('fill', 'steelblue')
-    .attr('d', geoPath);
+    console.log('countries: ', countries);
+
+    svg.selectAll('path')
+      .data(countryFeatures).enter()
+      .append('path')
+      .attr('class', 'feature')
+      .style('fill', 'steelblue')
+      .attr('d', geoPath);
+    
+    // put boarder around states 
+    svg.append('path')
+      .datum(topojson.mesh(worldData, worldData.objects.countries, function(a, b) { return a !== b; }))
+      .attr('class', 'mesh')
+      .attr('d', geoPath);
+    
+    // add circles to svg
+    // const visitedLocationCoordinates = [
+    //   [],
+    //   []
+    // ];
   
-  // put boarder around states 
-  svg.append('path')
-    .datum(topojson.mesh(worldData, worldData.objects.countries, function(a, b) { return a !== b; }))
-    .attr('class', 'mesh')
-    .attr('d', geoPath);
-  
-  // add circles to svg
-  // svg.selectAll('circle')
-  //   .data([aa,bb]).enter()
-  //   .append('circle')
-  //   .attr('cx', function (d) { console.log(projection(d)); return projection(d)[0]; })
-  //   .attr('cy', function (d) { return projection(d)[1]; })
-  //   .attr('r', '8px')
-  //   .attr('fill', 'red');
+    // svg.selectAll('circle')
+    //   .data(visitedLocationCoordinates).enter()
+    //   .append('circle')
+    //   .attr('cx', function (d) { console.log(projection(d)); return projection(d)[0]; })
+    //   .attr('cy', function (d) { return projection(d)[1]; })
+    //   .attr('r', '8px')
+    //   .attr('fill', 'red');
   }
 
   constructor(private renderer: Renderer2) { }
