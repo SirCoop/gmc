2b9cc3be7f25743f83655134a80674875c5a3c3b
Gary Cooper <cooper.garym@gmail.com>
2018-07-22 12:04:44 -0400

    organized visited locations.

--- src/app/components/worldmap/visited-locations.ts
+++ src/app/components/worldmap/visited-locations.ts
@@ -1,49 +1,73 @@
 // [long, lat]
 const visitedLocationCoordinates: any = [
-    // San Diego, California
-    [-117.161084, 32.715738],
-    // Slovakia
-    [18.076376, 48.306141],
-    // Paris, France
-    [2.352222, 48.856614],
-    // Amsterdam, Netherlands
-    [4.895168, 52.370216],
-    // Tokyo, Japan
-    [139.691706, 35.689487],
-    // Okinawa, Japan
-    [127.945404, 26.501301],
-    // Vienna, Austria
-    [16.373819, 48.208174],
-    // Munich, Germany
-    [11.581980, 48.135125],
+    /* North America */
     // Toronto, Canada
     [-79.383184, 43.653226],
     // Kingston, Ontario, Canada
     [-76.485954, 44.231172],
+    // South Miami Beach, FL
+    [-80.140322, 25.774949],
+    // Charlotte, NC
+    [-80.843127, 35.227087],
+    // San Diego, California
+    [-117.161084, 32.715738],
+    // Seattle
+    [-122.332071, 47.606209],
+
+    /* Central America */
+    // Dominical, Cost Rica
+    [-83.859231, 9.252431],
+    // Tijuana, Mexico
+    [-117.038247, 32.514947],
+
+    /* South America */
     // Medellin, Colombia
     [-75.581212, 6.244203],
+    // Bogota, Colombia
+    [-74.072092, 4.710989],
     // Rio de Janeiro
     [-43.172896, -22.906847],
+
+    /* Europe */
+    // Nitra, Slovakia
+    [18.076376, 48.306141],
+    // Budapest, Hungary
+    [19.040235, 47.497912],
+    // Paris, France
+    [2.352222, 48.856614],
+    // Amsterdam, Netherlands
+    [4.895168, 52.370216],
+    // Vienna, Austria
+    [16.373819, 48.208174],
+    // Munich, Germany
+    [11.581980, 48.135125],
+    // Venice, Italy
+    [12.315515, 45.440847],
+    // Pag, Croatia
+    [14.991688, 44.467219],
+
+    /* East Asia */
+    // Tokyo, Japan
+    [139.691706, 35.689487],
+    // Okinawa, Japan
+    [127.945404, 26.501301],
+    // Osan, Korea
+    [127.077221, 37.149810],
+
+    /* SouthEast Asia */  
     // Chalong, Phuket, Thailand
     [98.348390, 7.836717],
     // Nusa Dua, Bali, Indonesia
     [115.230162, -8.794096],
     // Kuala Lumpur, Malaysia
     [101.686855, 3.139003],
-    // Dominical, Cost Rica
-    [-83.859231, 9.252431],
-    // Tijuana, Mexico
-    [-117.038247, 32.514947],
-    // Pag, Croatia
-    [14.991688, 44.467219],
-    // Waikiki, Hawaii
-    [-157.835698, 21.285002],
-    // Guam, Micronesia
-    [144.800266, 13.513616],
-    // South Miami Beach, FL
-    [-80.140322, 25.774949],
-    // Charlotte, NC
-    [-80.843127, 35.227087]
+
+    /* South Pacific */
+     // Waikiki, Hawaii
+     [-157.835698, 21.285002],
+     // Guam, Micronesia
+     [144.800266, 13.513616] 
+    
   ];
 
   export default visitedLocationCoordinates;
