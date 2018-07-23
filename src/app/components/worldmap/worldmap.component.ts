9b4f6e8675eaa4643b02dc225a4f2b9e6fdde26d
Gary Cooper <cooper.garym@gmail.com>
2018-07-23 09:30:18 -0400

    test

--- src/app/components/worldmap/worldmap.component.ts
+++ src/app/components/worldmap/worldmap.component.ts
@@ -46,13 +46,15 @@ export class WorldmapComponent implements OnInit {
     const width = 900 - margin.left - margin.right;
     const height = 700 - margin.top - margin.bottom;
    
-    const color = d3.scaleThreshold()
-      .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000]);
-      // .range([
-      //   'rgb(247,251,255)', 'rgb(222,235,247)', 'rgb(198,219,239)', 'rgb(158,202,225)',
-      // 'rgb(107,174,214)', 'rgb(66,146,198)','rgb(33,113,181)','rgb(8,81,156)',
-      // 'rgb(8,48,107)', 'rgb(3,19,43)'
-      // ]);
+    const color = d3.scaleThreshold<number, string>()
+      .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
+      .range([
+        'rgb(247,251,255)', 'rgb(222,235,247)', 'rgb(198,219,239)', 'rgb(158,202,225)',
+        'rgb(107,174,214)', 'rgb(66,146,198)', 'rgb(33,113,181)', 'rgb(8,81,156)',
+        'rgb(8,48,107)', 'rgb(3,19,43)'
+      ]);
+
+    console.log('color: ', color);
     
     /* projection definitions */
     const projection = d3.geoMercator()
