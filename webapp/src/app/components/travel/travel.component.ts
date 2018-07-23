331fbe41792663a6d6c697e094f431ad2913b6fe
Gary Cooper <cooper.garym@gmail.com>
2018-07-23 17:14:24 -0400

    restoring order

--- webapp/src/app/components/travel/travel.component.ts
+++ webapp/src/app/components/travel/travel.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TravelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('/api').subscribe(data => {
+      console.log('data: ', data);
+    });
 
   }
 
