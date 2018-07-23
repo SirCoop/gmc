c2151701516035f2501e33f788621a11b14542a1
Gary Cooper <cooper.garym@gmail.com>
2018-07-23 15:48:17 -0400

    housekeeping

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET api listing. */
+/* GET api listing */
 router.get('/', (req, res) => {
   res.send('api works');
 });
