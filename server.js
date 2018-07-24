91dfb4e7aec5e358326fb6e15c0b9bdd02157234
Gary Cooper <cooper.garym@gmail.com>
2018-07-24 11:42:02 -0400

    changed routers dir to routes.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 /* Get API routes */
-const api = require('./routers/apiRouter');
+const api = require('./routes/apiRouter');
 
 /* Set api routes */
 app.use('/api', api);
