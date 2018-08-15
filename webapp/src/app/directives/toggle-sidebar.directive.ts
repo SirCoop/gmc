b561d90db72eba6c7b34d573dd6ee71463bdab5d
Gary Cooper <cooper.garym@gmail.com>
2018-08-15 15:57:01 -0400

    landing-page sidenav toggle directive works

--- webapp/src/app/directives/toggle-sidebar.directive.ts
+++ webapp/src/app/directives/toggle-sidebar.directive.ts
@@ -1,32 +1,26 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, DoCheck } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Directive({
   selector: '[appToggleSidebar]'
 })
-export class ToggleSidebarDirective implements OnInit {
+export class ToggleSidebarDirective implements DoCheck {
   currentUrl = '';
 
   constructor(private el: ElementRef, private router: Router, private renderer: Renderer2) {
     
     router.events.subscribe((_: NavigationEnd) => {
-      this.currentUrl = _.url;
-      
-      console.log('this.currentUrl: ', this.currentUrl);
-      if (this.currentUrl === '/' || this.currentUrl === 'undefined') {
-        this.el.nativeElement.style.visibility = 'hidden';
-      } else {
-        this.el.nativeElement.style.visibility = 'visible';
-      }
+      this.currentUrl = _.url;        
      });
   }
 
-  ngOnInit() {    
+  ngDoCheck() {
     if (this.currentUrl === '/' || this.currentUrl === 'undefined') {
       this.el.nativeElement.style.visibility = 'hidden';
     } else {
       this.el.nativeElement.style.visibility = 'visible';
     }
   }
+  
 }
 
