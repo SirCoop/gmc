0f11cad148a0787af78f2674e729128808e268cf
Gary Cooper <cooper.garym@gmail.com>
2018-08-16 16:08:21 -0400

    styling

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MatSidenav } from '../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,27 @@ export class AppComponent {
   title = 'app';
   currentUrl = '';
   hideSideNav = true;
+  ipadWidth = 768;
+  ipadHeight = 1024;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
+
   constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
     router.events.subscribe((_: NavigationEnd) => {
       this.currentUrl = _.url;
+      // force mat-sidenav to close after clicking icon on mobile
+      if (this.isScreenSmall()) {
+        this.sidenav.close();
+      }      
     });
+  }
+
+  isScreenSmall(): boolean {
+    return window.innerWidth < this.ipadWidth && window.innerHeight < this.ipadHeight;
   }
 }
