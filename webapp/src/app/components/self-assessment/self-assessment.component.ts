b1dfa44b2517e4ebec73759245e57b1a83414671
Gary Cooper <cooper.garym@gmail.com>
2018-08-17 14:40:12 -0400

    self-assessment page.

--- webapp/src/app/components/self-assessment/self-assessment.component.ts
+++ webapp/src/app/components/self-assessment/self-assessment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, AfterContentChecked } from '@angular/core';
+import { MatTabChangeEvent } from '../../../../node_modules/@angular/material';
 import frontEndSkills from './front-end.skills';
 import backEndSkills from './back-end.skills';
 import machineLearningSkills from './machine-learning.skills';
@@ -8,13 +9,14 @@ import machineLearningSkills from './machine-learning.skills';
   templateUrl: './self-assessment.component.html',
   styleUrls: ['./self-assessment.component.scss']
 })
-export class SelfAssessmentComponent implements OnInit {
+export class SelfAssessmentComponent implements OnInit, AfterContentChecked {
 
   dataSource = [frontEndSkills, backEndSkills, machineLearningSkills];
 
   pdfTitle: string;
-  pdfSrc = '../../../assets/predictive_index.pdf';
-  zoom = 1.05;
+  pdfSrc1 = '../../../assets/predictive_index.pdf';
+  pdfSrc2 = '../../../assets/mb_entp_description.pdf';
+  zoom = 1.15;
   originalSize = false;
   showAll = true;
   renderText = false;
@@ -23,26 +25,35 @@ export class SelfAssessmentComponent implements OnInit {
 
   isMobile = true;
 
-  // iframeLoaded() {
-  //   const iFrameID = document.getElementById('mb') as HTMLIFrameElement;
-  //     if (iFrameID) {
-  //           iFrameID.height = '';
-  //           iFrameID.width = '';
-  //           // iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + 'px';
-  //           // iFrameID.width = iFrameID.contentWindow.document.body.scrollWidth + 'px';
-  //     }   
-    
-  // }
+  tab: string;
+
+  iframeLoaded() {
+    const iFrameID = document.getElementById('mb') as HTMLIFrameElement;
+    if (iFrameID) {
+      const el = iFrameID.contentWindow.document.getElementsByTagName('body')[0].addEventListener('click', event => {
+        event.preventDefault();
+      });
+    }
+
+  }
   @HostListener('window:resize', ['$event'])
-  onResize(event) {
+  onResize() {
     this.isMobile = window.innerWidth <= 980;
   }
 
   constructor() { }
 
   ngOnInit() {
-    // this.iframeLoaded();
     this.isMobile = window.innerWidth <= 980;
   }
 
+  ngAfterContentChecked() {
+    if (this.tab === 'Personality Assessment') {
+      this.iframeLoaded();
+    }
+  }
+
+  onTabClick(event: MatTabChangeEvent): void {    
+    this.tab = event.tab.textLabel;    
+  }
 }
