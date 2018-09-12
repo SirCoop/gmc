43a5818d37cae1576b32f0db3d35f2e0a9fdf35b
Gary Cooper <cooper.garym@gmail.com>
2018-09-12 19:48:36 -0400

    updated email template.

--- services/contact.service.js
+++ services/contact.service.js
@@ -34,6 +34,9 @@ module.exports = {
       text: '',
       html: `<hr>
             <h2>Letter from ${postBody.firstName} ${postBody.lastName}</h2>
+            <p>Organization: ${postBody.organization}</p>
+            <p>Location: ${postBody.city}, ${postBody.state}, ${postBody.country}</p>
+            <p>Phone: ${postBody.phone}</p>
             <p><b>Received:</b> ${postBody.date}</p>
             <p><b>Message:</b> ${postBody.comment}</p>
             <hr>
