62e5dbcfba3cd12660b0bf157b0ee0dcfff423a2
Gary Cooper <cooper.garym@gmail.com>
2018-09-15 12:53:49 -0400

    dashboard and widget config working.

--- webapp/src/app/components/draggable-desktop/models.ts
+++ webapp/src/app/components/draggable-desktop/models.ts
@@ -14,39 +14,30 @@ export interface IWidgetDashboard {
     Widgets: WidgetMetaData[];
 }
 
-// export const containerConfig = {
-//     'margins': [5],
-//     'draggable': true,
-//     'resizable': false,
-//     'max_cols': 4,
-//     'max_rows': 0,
-//     'visible_cols': 0,
-//     'visible_rows': 0,
-//     'min_cols': 1,
-//     'min_rows': 1,
-//     'col_width': 2,
-//     'row_height': 2,
-//     'cascade': 'left',
-//     'min_width': 50,
-//     'min_height': 50,
-//     'fix_to_grid': false,
-//     'auto_style': true,
-//     'auto_resize': false,
-//     'maintain_ratio': false,
-//     'prefer_new': false,
-//     'zoom_on_drag': false,
-//     'limit_to_screen': false,
-//     'allow_overlap': false,
-//     'widget_width_factor': 22
-// };
-
 export const containerConfig = {
-    'margins': [1],
+    'margins': [5],
     'draggable': true,
-    'max_cols': 4,
-    'max_rows': 0,
-    'min_cols': 1,
-    'min_rows': 1,
+    'resizable': false,
+    // 'max_cols': 4,
+    // 'max_rows': 0,
+    // 'visible_cols': 0,
+    // 'visible_rows': 0,
+    // 'min_cols': 1,
+    // 'min_rows': 1,
+    // 'col_width': 2,
+    // 'row_height': 2,
+    // 'cascade': 'left',
+    // 'min_width': 50,
+    // 'min_height': 50,
+    // 'fix_to_grid': false,
+    // 'auto_style': true,
+    // 'auto_resize': false,
+    // 'maintain_ratio': false,
+    // 'prefer_new': false,
+    // 'zoom_on_drag': false,
+    // 'limit_to_screen': false,
+    // 'allow_overlap': false,
+    // 'widget_width_factor': 22
 };
 
 export const widgetmetadatas = [
